2ec8ef498b7c0e52aa552f70f89e88fba750a3ad
Virginia224 <47594162+Virginia224@users.noreply.github.com>
2020-03-29 17:45:16 +0200

    Update threshold_netmeta.R

--- R/threshold_netmeta.R
+++ R/threshold_netmeta.R
@@ -6,42 +6,41 @@
 ####                        is the max treat. effect (e.g. for
 ####                        beneficial outcomes) or the minimum
 ####                        (e.g. for harmful outcomes)
-####                decision (default "decision"), decision rule 
+####                decision (default "decision"), decision rule
 ####                        based on MCID; if "change" then use
-####                        maximum efficacy rule but only changes 
+####                        maximum efficacy rule but only changes
 ####                        the decision when alternative treatment
 ####                        becomes more effective than base case by mcid or more
-####                mcid (default 0 i.e. max efficacy decision rule), 
+####                mcid (default 0 i.e. max efficacy decision rule),
 ####                        minimally clinical important difference for the decision (if "decision")
 ####                        or for changing the decision (if "change")
 library(nmathresh)
 
 
 threshold_netmeta <- function(nma, nma.type="random", opt.max=T, decision="decision", mcid=0) {
-  
+
   K <- nma$n  # Number of treatments
-  
-  source("matrix.map.R")
-  X <- design(nma)  ## check you get same design matrix as above
-  
+
+  X <- matrix.map(nma)  ## check you get same design matrix as above
+
   if (nma.type=="random") {
     # create basic parameters based on chosen reference
     basicparam<-nma$TE.random[-1, 1] #the basic parameters all vs treat 1
-    covariance<-nma$Cov.random[1:(K-1), 1:(K-1)] # the variance-covariance between the basic parameters 
+    covariance<-nma$Cov.random[1:(K-1), 1:(K-1)] # the variance-covariance between the basic parameters
   }
   else {
     # create basic parameters based on chosen reference
     basicparam<-nma$TE.fixed[-1, 1] #the basic parameters all vs treat 1
-    covariance<-nma$Cov.fixed[1:(K-1), 1:(K-1)] # the variance-covariance between the basic parameters 
+    covariance<-nma$Cov.fixed[1:(K-1), 1:(K-1)] # the variance-covariance between the basic parameters
   }
-    
+
   lik.cov<-recon_vcov(covariance, X = X) #reconstruct the variance
-  
-  thresh <- nma_thresh(mean.dk = basicparam, 
-                       lhood = lik.cov, 
-                       post = covariance, 
-                       nmatype = "fixed", X = X, 
-                       trt.code =  nma$trts, opt.max = opt.max, 
+
+  thresh <- nma_thresh(mean.dk = basicparam,
+                       lhood = lik.cov,
+                       post = covariance,
+                       nmatype = "fixed", X = X,
+                       trt.code =  nma$trts, opt.max = opt.max,
                        mcid = mcid, mcid.type = decision)
 
   return(thresh)
