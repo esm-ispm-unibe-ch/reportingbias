1401e9d56f6d0681de790659fef1d0f8bcc718f9
Virginia224 <47594162+Virginia224@users.noreply.github.com>
2021-01-18 12:35:28 +0100

    Update nmafunnel.R

--- R/nmafunnel.R
+++ R/nmafunnel.R
@@ -38,7 +38,7 @@ nmafunnel <- function(nma, small.values="good") {
       for (j in 1:ncol(t)) {
         if(t[i,j]>9 & i<j) {
           if (nma$sm=="OR" | nma$sm=="RR") {
-            ma <- metabin(event1,n1,event2,n2, subset = nma$treat1==rownames(t)[i] & nma$treat2==colnames(t)[j], data=nma$data)
+            ma <- metabin(event1,n1,event2,n2, subset = nma$treat1==rownames(t)[i] & nma$treat2==colnames(t)[j], data=nma$data, sm=nma$sm)
             funnel(ma, contour = c(0.9, 0.95, 0.99), col.contour = c("darkred", "red", "lightcoral"),
                    cex=1.5, col="darkblue", bg="blue", cex.lab=1.2, xlab = paste(ma$sm, "effect reported as", rownames(t)[i], "over", colnames(t)[j]))
             legend("topright", c("0.1 > p > 0.05", "0.05 > p > 0.01", "< 0.01"), fill = c("darkred", "red", "lightcoral"), bg="white")
@@ -46,7 +46,7 @@ nmafunnel <- function(nma, small.values="good") {
             mb <- metabias(ma, method.bias = "score")
           }
           else {
-            ma <- metagen(nma$TE,nma$seTE,subset = nma$treat1==rownames(t)[i] & nma$treat2==colnames(t)[j], sm=sm)
+            ma <- metagen(nma$TE,nma$seTE,subset = nma$treat1==rownames(t)[i] & nma$treat2==colnames(t)[j], sm=nma$sm)
             funnel(ma, contour = c(0.9, 0.95, 0.99), col.contour = c("darkred", "red", "lightcoral"),
                    cex=1.5, col="darkblue", bg="blue", cex.lab=1.2, xlab = paste(ma$sm, "effect reported as", rownames(t)[i], "-", colnames(t)[j]))
             legend("topright", c("0.1 > p > 0.05", "0.05 > p > 0.01", "< 0.01"), fill = c("darkred", "red", "lightcoral"), bg="white")
